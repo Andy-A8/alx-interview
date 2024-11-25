d6b3acb16896cd5773069c371e2c04175d0dc2fd
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-11-25 14:34:41 -0800

    Update solution

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -14,12 +14,12 @@ def rotate_2d_matrix(matrix):
 
     """ Transpose the matrix """
     for i in range(n):
-        for j in range(i + 1, n):
+        for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    """ Reverse each row """
-    for i in range(n):
-        matrix[i].reverse()
 
-    """ Print matrix in 2D format """
+    """ Reverse each row """
+    for row in matrix:
+        row.reverse()
+
     for row in matrix:
         print(row)
