17cda59039b07a7fc2d29d1f53f1a493d305e44f
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-12-09 21:52:11 +0000

    Updated solution

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -10,7 +10,7 @@ def isWinner(x, nums):
         primes = [True] * (n + 1)
         primes[0] = primes[1] = False  # 0 and 1 are not prime numbers
         for i in range(2, int(n ** 0.5) + 1):
-            if prime[i]:
+            if primes[i]:
                 for j in range(i * i, n + 1, i):
                     primes[j] = False
         return [i for i in range(2, n + 1) if primes[i]]
